4f560784e45440c0f143dbc3210fa2763b4c33ec
jpswt <joel_swt@yahoo.com>
2022-11-16 22:34:50 -0600

    works

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import LoginBtn from './components/LoginBtn';
 import LogoutBtn from './components/LogoutBtn';
+import Profile from './components/Profile';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+	const { isLoading, error } = useAuth0();
+
 	return (
 		<div className="App">
 			<h1>Login</h1>
-			<LoginBtn />
-			<LogoutBtn />
+			{error && <p>Authentication Error</p>}
+			{!error && isLoading && <p>Loading....</p>}
+			{!error && !isLoading && (
+				<>
+					<LoginBtn />
+					<LogoutBtn />
+					<Profile />
+				</>
+			)}
 		</div>
 	);
 }
