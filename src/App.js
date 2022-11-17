93e91b8ee098ac6103c7287ca2b1fb23a54258e6
jpswt <joel_swt@yahoo.com>
2022-11-16 22:16:59 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginBtn from './components/LoginBtn';
+import LogoutBtn from './components/LogoutBtn';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Login</h1>
+			<LoginBtn />
+			<LogoutBtn />
+		</div>
+	);
 }
 
 export default App;
